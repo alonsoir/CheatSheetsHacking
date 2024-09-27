ad1501f670b95b4e2ed831e717138cf134075d47
Alonso Isidoro Román <alonsoir@gmail.com>
2024-09-27 12:56:12 +0200

    added wasap-summarizer.py

---
+++ wasap-summarizer.py
@@ -0,0 +1,62 @@
+from pytube import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi
+from langchain_openai import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.schema import Document
+import os
+import openai
+import requests
+
+# Configura la API de OpenAI
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def summarize_video(url):
+    try:
+        print(f"Procesando la URL: {url}")
+        video_id = url.split("v=")[-1]
+        # Obtén la transcripción del video usando youtube-transcript-api
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+
+        # Combina los textos de la transcripción
+        transcript_text = " ".join([entry['text'] for entry in transcript])
+
+        # Divide el texto en fragmentos
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        split_texts = text_splitter.split_text(transcript_text)
+
+        # Convertir textos a documentos
+        documents = [Document(page_content=text) for text in split_texts]
+
+        llm = OpenAI(temperature=0.7, max_tokens=500)
+        chain = load_summarize_chain(llm, chain_type="map_reduce")
+
+        # Genera el resumen del video usando invoke
+        summary = chain.invoke({"input_documents": documents})
+        # print(f"Resumen generado: {summary}")
+
+        send_whatsapp_message(url,summary)
+
+    except Exception as e:
+        print(f"Ups!, Ocurrió un error: {e}")
+
+
+def send_whatsapp_message(url, message):
+    api_key = os.getenv("CALLMEBOT_API_KEY")  # Reemplaza con tu API Key obtenida de CallMeBot
+    phone_number = os.getenv("MY_PHONE_NUMBER")  # Tu número de WhatsApp con código de país, por ejemplo:  +1234567890
+    text_message = f"resume from {url}\n{message['output_text']}"  # El mensaje que se va a enviar
+
+    url = f"https://api.callmebot.com/whatsapp.php?phone={phone_number}&text={text_message}&apikey={api_key}"
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("Mensaje enviado correctamente a WhatsApp.")
+        else:
+            print(f"Error al enviar el mensaje: {response.status_code}")
+    except Exception as e:
+        print(f"Ups! Ocurrió un error: {e}")
+
+if __name__ == "__main__":
+    url = input("Ingresa la URL del video de YouTube: ")
+    summarize_video(url)
